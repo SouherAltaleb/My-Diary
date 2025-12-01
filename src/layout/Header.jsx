323d7bd3eb7c9620facce061952599cc49c93718
Souher Al-Taleb <altalebsouher8@gmail.com>
2025-12-01 22:26:19 +0100

    h1 size angepasst

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -8,7 +8,7 @@ export default function Header({ onAddClick }) {
       <div className="flex items-center gap-3 max-auto px-6 py-3 ">
         <img src={logo} alt="My Diary logo" className="w-25 object-contain" />
 
-        <h1 className="drop-shadow font-bold">MyDiary App</h1>
+        <h1 className="drop-shadow font-bold text-3xl pt-5">MyDiary App</h1>
       </div>
       {/* onAdd kommt von der Seite home */}
       <AddEntryButton onClick={onAddClick} />
